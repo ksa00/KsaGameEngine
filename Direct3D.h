9482f3888c3a38c938d4caf22409575645408eab
KRIPESH SAPKOTA <230587@jc-21.jp>
2024-05-21 16:41:33 +0900

    プロジェクト ファイルを追加します。

---
+++ Direct3D.h
@@ -0,0 +1,29 @@
+#pragma once
+//インクルード
+#include <d3d11.h>
+#pragma comment(lib, "d3dcompiler.lib")
+
+//リンカ
+#pragma comment(lib, "d3d11.lib")
+
+namespace Direct3D
+{
+	extern ID3D11Device* pDevice;
+	extern ID3D11DeviceContext* pContext;
+
+	//初期化
+	void Initialize(int winW, int winH, HWND hWnd);
+
+	//シェーダー準備
+	void InitShader();
+
+	//描画開始
+	void BeginDraw();
+
+	//描画終了
+	void EndDraw();
+
+	//解放
+	void Release();
+};
+
